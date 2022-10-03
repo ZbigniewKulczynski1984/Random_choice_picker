30f227c4a0f5ba3eae484c0da02184543160069e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-03 10:56:50 +0200

    code added to app.js

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ textarea.addEventListener('keyup', (e) => {
 })
 
 function createTags(input) {
-    
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    });
 }
+
